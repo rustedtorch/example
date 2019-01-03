df40db18d0e30bd3d5b64df1f97c5e812737cb8b
Adrian Punga <adrian.punga@ejobsgroup.ro>
2019-01-04 00:19:00 +0200

    Clean-up

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,7 @@
 #[macro_use]
 extern crate torch;
 
-use torch::tensor::Tensor;
-
 fn main() {
-    match test() {
-        Err(message) => panic!(message),
-        _ => (),
-    };
-}
-
-pub fn test() -> Result<(), &'static str> {
-    let x = Tensor::new_from_vector(vec![1.0, 2.01, 3.02, 4.5]);
-    let y = x.add(Tensor::new_from_vector(vec![0.1, 0.2, 0.3, 0.4]))?;
-    let z = Tensor::new_from_cube(vec![
-        vec![
-            vec![1.0, 2.01, 3.02, 4.5],
-            vec![1.0, 2.01, 3.02, 4.5],
-            vec![1.0, 2.01, 3.02, 4.5],
-            vec![1.0, 2.01, 3.02, 4.5],
-        ],
-        vec![
-            vec![1.0, 2.01, 3.02, 4.5],
-            vec![1.0, 2.01, 3.02, 4.5],
-            vec![1.0, 2.01, 3.02, 4.5],
-            vec![1.0, 2.01, 3.02, 4.5],
-        ],
-    ]);
     let w = tensor!([
         [
             [
@@ -90,5 +65,4 @@ pub fn test() -> Result<(), &'static str> {
         ]
     ]);
     println!("{:?}", w);
-    Ok(())
 }
